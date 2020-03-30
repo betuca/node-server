97e9d8dbefab94e6e6417a6412048fccc6290355
Betuca <betuca69@gmail.com>
2020-03-30 11:41:15 -0300

    Add authentication flow usging goolge oAuth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,4 +4,14 @@ module.exports = app => {
 	app.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 
 	app.get("/auth/google/callback", passport.authenticate("google"));
+
+	app.get("/api/logout", (req, res) => {
+		req.logout();
+		res.send("logged out. Saved user: " + req.user);
+	});
+
+	app.get("/api/current_user", (req, res) => {
+		console.log(req.user);
+		res.send(req.user);
+	});
 };
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => {
+		done(null, user);
+	});
+});
 
 passport.use(
 	new GoogleStrategy(
@@ -10,9 +23,25 @@ passport.use(
 			callbackURL: "/auth/google/callback"
 		},
 		(accessToken, refreshToken, profile, done) => {
-			console.log("access token", accessToken);
-			console.log("refresh token", refreshToken);
-			console.log("profile", profile);
+			// console.log("access token", accessToken);
+			// console.log("refresh token", refreshToken);
+			// console.log("profile", profile);
+
+			User.findOne({ googleId: profile.id }).then(existingUser => {
+				if (existingUser) {
+					console.log("welcome back, ", existingUser.name);
+					done(null, existingUser);
+				} else {
+					console.log("welcome, ", profile.name);
+					new User({
+						googleId: profile.id,
+						name: profile.displayName,
+						email: profile.emails[0].value
+					})
+						.save()
+						.then(user => done(null, existingUser));
+				}
+			});
 		}
 	)
 );
